ec81c672f73728e5b1dcb43e4f10df3ffdaa617a
tiagosutter <tiagosutter@gmail.com>
2023-04-06 17:12:15 -0300

    Remove TestingDependencyInjection.kt

--- app/src/main/java/br/dev/tiagosutter/foodtracker/ui/SecondFragment.kt
+++ app/src/main/java/br/dev/tiagosutter/foodtracker/ui/SecondFragment.kt
@@ -1,25 +1,20 @@
 package br.dev.tiagosutter.foodtracker.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import br.dev.tiagosutter.foodtracker.R
-import br.dev.tiagosutter.foodtracker.TestingDependencyInjection
 import br.dev.tiagosutter.foodtracker.databinding.FragmentSecondBinding
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
     private val binding get() = _binding!!
-
-    @Inject
-    lateinit var testingDependencyInjection: TestingDependencyInjection
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,7 +28,6 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        testingDependencyInjection.logSomeString()
 
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
