20e1ab7458e17e9fb74d64e22268c7535fdfb556
tiagosutter <tiagosutter@gmail.com>
2023-04-12 12:20:44 -0300

    Add save button callback to NewFoodEntryFragment.kt

--- app/src/main/java/br/dev/tiagosutter/foodtracker/ui/NewFoodEntryFragment.kt
+++ app/src/main/java/br/dev/tiagosutter/foodtracker/ui/NewFoodEntryFragment.kt
@@ -1,10 +1,12 @@
 package br.dev.tiagosutter.foodtracker.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import br.dev.tiagosutter.foodtracker.R
 import br.dev.tiagosutter.foodtracker.databinding.FragmentNewFoodEntryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +20,24 @@ class NewFoodEntryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        val menuHost = requireActivity() as MenuHost
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // no-op
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.action_save -> {
+                        Toast.makeText(requireContext(), "SAVE from fragment", Toast.LENGTH_SHORT)
+                            .show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        })
 
         _binding = FragmentNewFoodEntryBinding.inflate(inflater, container, false)
         return binding.root
