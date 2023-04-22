b40a7339d5c424dc3f850deb42055ca8f10be7d0
tiagosutter <tiagosutter@gmail.com>
2023-04-21 21:54:40 -0300

    Setup firebase analytics

--- app/src/main/java/br/dev/tiagosutter/foodtracker/ui/MainActivity.kt
+++ app/src/main/java/br/dev/tiagosutter/foodtracker/ui/MainActivity.kt
@@ -12,12 +12,14 @@ import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import br.dev.tiagosutter.foodtracker.NotificationScheduler
 import br.dev.tiagosutter.foodtracker.R
 import br.dev.tiagosutter.foodtracker.databinding.ActivityMainBinding
 import br.dev.tiagosutter.foodtracker.ui.entrieslist.FoodEntriesFragmentDirections
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -29,9 +31,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var analytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initializeFirebaseAnalytics()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -70,6 +74,14 @@ class MainActivity : AppCompatActivity() {
         } else {
             notificationScheduler.schedule()
         }
+    }
+
+    private fun initializeFirebaseAnalytics() {
+        // Initialize Firebase Analytics so it starts automatically logging
+        //  some events and user properties like
+        //   https://support.google.com/firebase/answer/9234069?hl=en
+        //   https://support.google.com/firebase/answer/9268042?hl=en
+        analytics = Firebase.analytics
     }
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
