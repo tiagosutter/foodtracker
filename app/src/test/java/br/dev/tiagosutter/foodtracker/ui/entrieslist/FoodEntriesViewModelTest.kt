55154c25498c8b198f0aa8f9dc4e86eeaf513ce9
tiagosutter <tiagosutter@gmail.com>
2023-04-30 20:28:30 -0300

    Intentional mistake on FoodEntriesViewModelTest.kt to test pipeline

--- app/src/test/java/br/dev/tiagosutter/foodtracker/ui/entrieslist/FoodEntriesViewModelTest.kt
+++ app/src/test/java/br/dev/tiagosutter/foodtracker/ui/entrieslist/FoodEntriesViewModelTest.kt
@@ -81,7 +81,7 @@ class FoodEntriesViewModelTest {
     @Test
     fun getAllEntries_entriesExist_viewStateGetsExpectedEntries() {
         // Arrange
-        setupToHaveEntires()
+//        setupToHaveEntires()
 
         // Act
         viewModel.getAllEntries()
