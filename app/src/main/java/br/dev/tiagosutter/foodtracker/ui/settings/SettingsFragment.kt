6f915ab503ab018ad34a028d2ba8a3cba9df9d29
tiagosutter <tiagosutter@gmail.com>
2023-05-08 19:23:00 -0300

    Implements permission rationale

--- app/src/main/java/br/dev/tiagosutter/foodtracker/ui/settings/SettingsFragment.kt
+++ app/src/main/java/br/dev/tiagosutter/foodtracker/ui/settings/SettingsFragment.kt
@@ -1,14 +1,14 @@
 package br.dev.tiagosutter.foodtracker.ui.settings
 
 import android.Manifest
+import android.app.AlertDialog
 import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.SwitchPreference
 import androidx.preference.SwitchPreferenceCompat
 import br.dev.tiagosutter.foodtracker.R
 import br.dev.tiagosutter.foodtracker.notifications.NotificationScheduler
@@ -19,6 +19,8 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class SettingsFragment : PreferenceFragmentCompat() {
 
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
     @Inject
     lateinit var notificationScheduler: NotificationScheduler
 
@@ -27,21 +29,43 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        val preference: SwitchPreferenceCompat? = findPreference<SwitchPreferenceCompat>("notify_daily")
+        val preference: SwitchPreferenceCompat? =
+            findPreference<SwitchPreferenceCompat>("notify_daily")
 
-        val requestPermissionLauncher =
+        requestPermissionLauncher =
             registerForActivityResult(
                 ActivityResultContracts.RequestPermission()
             ) { isGranted: Boolean ->
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+                    return@registerForActivityResult
+                }
+                val shouldShowRationale = ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.POST_NOTIFICATIONS
+                )
                 if (isGranted) {
                     preference?.isChecked = true
                     notificationScheduler.schedule()
+                } else if (shouldShowRationale) {
+                    AlertDialog.Builder(requireContext())
+                        .setNegativeButton(R.string.retry) { dialog, _ ->
+                            requestNotificationPermission(requestPermissionLauncher)
+                            dialog.dismiss()
+                        }
+                        .setPositiveButton(R.string.i_am_sure) { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .setTitle(R.string.notification_permission)
+                        .setMessage(R.string.are_you_sure_no_notification_permission)
+                        .show()
                 } else {
-                    Toast.makeText(
-                        requireContext(),
-                        getString(R.string.could_not_enable_notifications_permission),
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    AlertDialog.Builder(requireContext())
+                        .setTitle(R.string.notification_permission)
+                        .setMessage(R.string.could_not_enable_notifications_permission)
+                        .setPositiveButton("OK") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .show()
                 }
             }
 
@@ -52,7 +76,6 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 true
             } else {
                 val schedulingResult = notificationScheduler.schedule()
-
                 if (schedulingResult == NotificationScheduler.SchedulingResult.NO_PERMISSION) {
                     requestNotificationPermission(requestPermissionLauncher)
                     false
@@ -65,8 +88,13 @@ class SettingsFragment : PreferenceFragmentCompat() {
         preference?.onPreferenceChangeListener = preferenceChangeListener
     }
 
+
     private fun requestNotificationPermission(requestPermissionLauncher: ActivityResultLauncher<String>) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val shouldShowRationale = ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.POST_NOTIFICATIONS
+            )
             requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         }
     }
